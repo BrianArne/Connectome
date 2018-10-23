1070265fec76473cc27b594a6baaaf20eab92e47
Brian Arne <baarne@g.cofc.edu>
2018-10-23 12:01:24 -0400

    Total Nodes Method Created
    Total nodes method has been added to the Brian Test Files directory. The
    method takes a file name with a variable that matches the file name and
    just returns the total number of nodes in the matlab file.

--- Brian Test Files/Verify.py
+++ Brian Test Files/Verify.py
@@ -1,36 +1,30 @@
 from scipy import io
 
-
-'''Extracts matlab matricies'''
-file_name = "Test"
-var = io.loadmat("Test.mat")
-
-
 '''
-Gets access to child node. Can remove the changing value to just get the length
-Changes on a 1:5 ratio
+Gets the total number of nodes in the matlab file
 '''
-#print(var["Test"])
-child_nodes = var[file_name][0,5]['child_nodes']
-#                              ^somehow controls the block of child_nodes
-print(child_nodes[0][0][1][0])
-#                       ^is the index of the child nodes
+def total_nodes(file_name): #Do not include .mat extension
+    file_with_extension = file_name + '.mat'
+    var = io.loadmat(file_with_extension, squeeze_me=True)
+    return len(var[file_name])
+
+print(total_nodes('Test'))
+print(total_nodes('T'))
 
 
+#TODO Layer Method, current node method, child nodes of current node method
 '''
 Gets access to the current node. Changes on a 1:1 ratio
-'''
-current_node = var[file_name][0,1]['current_node']
+current_node = var[file_name][0,15]['current_node']
 #                               ^controls current node number
 print(current_node[0][0][0][0])
 #                           ^extracts value
 
 
-'''
 Gets access to the layer of the current node.
 Changes on a 1:len(child_node) ratio
-'''
 layer = var[file_name][0,10]['layer']
 #                         ^extracts layer but sorts in blocks of 5 (0,5,10,15)
 print(layer[0][0][0][0])
 #                    ^extracts value
+'''
