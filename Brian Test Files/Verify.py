71d1438daab0972fd29b44de746f540e0cc33104
workstation6 <workstation6@localhost.localdomain>
2018-10-24 13:13:11 -0400

    Get_node_layer method added

--- Brian Test Files/Verify.py
+++ Brian Test Files/Verify.py
@@ -8,11 +8,38 @@ def total_nodes(file_name): #Do not include .mat extension
     var = io.loadmat(file_with_extension, squeeze_me=True)
     return len(var[file_name])
 
-print(total_nodes('Test'))
-print(total_nodes('T'))
+'''
+Returns a ndarray of node data
+'''
+def access_node_data(file_name, node):
+
+  file_with_extension = file_name + '.mat'
+  var = io.loadmat(file_with_extension, squeeze_me=True)
+  data = var[file_name][node]
+  return data
+
+'''
+Returns the layer of the node
+'''
+def get_layer_of_node(node_data):
+  a = node_data.tolist()
+  return a[0]
 
 
-#TODO Layer Method, current node method, child nodes of current node method
+
+#Testing Garbage
+num = 11
+data = access_node_data('Test', num)
+print(get_layer_of_node(data))
+#print(data.index(0))
+
+
+
+
+#TODO Current node method, child nodes of current node method
+
+
+
 '''
 Gets access to the current node. Changes on a 1:1 ratio
 current_node = var[file_name][0,15]['current_node']
@@ -28,3 +55,4 @@ layer = var[file_name][0,10]['layer']
 print(layer[0][0][0][0])
 #                    ^extracts value
 '''
+
