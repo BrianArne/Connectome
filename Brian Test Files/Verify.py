d2afcf6fdaab4fcaa6d19b534ab5043e61cc1dc5
Brian Arne <baarne@g.cofc.edu>
2018-11-05 13:05:42 -0500

    New Methods Work With Matt's Old Code

--- Brian Test Files/Verify.py
+++ Brian Test Files/Verify.py
@@ -8,14 +8,6 @@ from scipy import io
 MATLAB_OFFSET = 1
 
 '''
-Gets the total number of nodes in the matlab file
-'''
-def total_nodes(file_name): #Do not include .mat extension
-    file_with_extension = file_name + '.mat'
-    var = io.loadmat(file_with_extension, squeeze_me=True)
-    return len(var[file_name])
-
-'''
 Returns a ndarray of node data
 '''
 def access_node_data(file_name, node):
@@ -26,19 +18,23 @@ def access_node_data(file_name, node):
   return data
 
 '''
-Returns the layer number of the node
-'''
-def get_layer_of_node(node_data):
-  a = node_data.tolist()
-  return a[0]
-
-'''
 Returns array of child nodes for a node
 '''
 def get_child_nodes(file_name, node):
   node_data = access_node_data(file_name, node)
   a = node_data.tolist()
-  return a[2]
+  arr = []
+  for val in a[2]:
+    arr.append(val)
+  return arr
+
+'''
+Returns actual node value
+'''
+def get_current_node(file_name, node):
+  node_data = access_node_data(file_name, node)
+  a = node_data.tolist()
+  return a[1]
 
 '''
 Returns dictionary layers and their lengths
@@ -54,8 +50,27 @@ def get_layer_lens(file_name):
       layer_dict[layer] = 1
   return layer_dict
 
+'''
+Returns the layer number of the node
+'''
+def get_layer_of_node(node_data):
+  a = node_data.tolist()
+  return a[0]
 
-'''Primitive Testing'''
+'''
+Gets the total number of nodes in the matlab file
+'''
+def total_nodes(file_name): #Do not include .mat extension
+    file_with_extension = file_name + '.mat'
+    var = io.loadmat(file_with_extension, squeeze_me=True)
+    return len(var[file_name])
+
+
+
+
+#######################
+###Primitive Testing###
+#######################
 
 #Testing globals
 brian_test = 'Test'
@@ -63,24 +78,28 @@ matt_test = 'T'
 num = 1 #must be a number < total_nodes() in both brian_test and matt_test
 
 print
-print("***Testing Total Nodes***")
+print("***Testing total_nodes()***")
 print('Total nodes in ' + brian_test + ': ' + str(total_nodes(brian_test)))
 print('Total nodes in ' + matt_test + ': ' + str(total_nodes(matt_test)))
 
 print
-print("***Testing Layer***")
+print("***Testing get_layer_of_node()***")
 matt_data = access_node_data(matt_test, num)
 brian_data = access_node_data(brian_test, num)
 print('Layer for node ' + str(num) + ' in ' + matt_test + ': ' + str(get_layer_of_node(matt_data)))
 print('Layer for node ' + str(num) + ' in ' + brian_test + ': ' + str(get_layer_of_node(brian_data)))
 
 print
-print("***Testing child nodes***")
+print("***Testing get_child_nodes()***")
 print('Child nodes for node ' + str(num) + ' in ' + matt_test + ': ' + str(get_child_nodes(matt_test, num)))
 print('Child nodes for node ' + str(num) + ' in ' + brian_test + ': ' + str(get_child_nodes(brian_test, num)))
 
 print
-print("***Testing Get Layer Lens***")
+print("***Testing get_layer_lens()***")
 print('Length of layers for ' + matt_test + ': ' + str(get_layer_lens(matt_test)))
 print('Length of layers for ' + brian_test + ': ' + str(get_layer_lens(brian_test)))
 
+print
+print("***Testing get_curret_node()***")
+print("Current node val for node 88 in 'T': " + str(get_current_node(matt_test, 88)))
+  
