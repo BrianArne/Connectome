3475efc4cbdbb456125074353503f390ca13a54d
Brian Arne <baarne@g.cofc.edu>
2018-11-16 12:46:05 -0500

    Fill matrix and Hash look up implemented

--- Brian Test Files/MatlabConnectome.py
+++ Brian Test Files/MatlabConnectome.py
@@ -44,23 +44,20 @@ class MatlabConnectome:
   '''
   Fills matrix from each node's data
   '''
-  def fill_matrix(self, max_child_node):
-    if self.matrix == None:
+  def fill_matrix(self):
+    if self._matrix == None:
       print("You need to construct an empty matrix first.")
     else:
       offset = 0
       layer = 1
-      for i in range(1, self.total_nodes()+1):
-        current_node = self.current_node(i)
-        child_node = self.child_nodes(i)
-        node_layer = self.layer_of_node(self.node_data(i))
-        if node_layer != layer:
-          offset += max_child_node[layer]
-          layer = node_layer
-        for j in child_node:
-          self.hash_lookup[current_node] = current_node + offset
-          self.matrix[current_node+offset][j+offset] = 1
-      return self.matrix
+      for n in self._nodes:
+        if n._layer != layer:
+          offset += self._layer_max_child[layer]
+          layer = n._layer
+        for j in n._input_nodes:
+          self._hash_lookup[n._current_number] = n._current_number + offset
+          self._matrix[n._current_number + offset][j+offset] = 1
+      return self._matrix
 
   '''
   Returns dictionary layers and their lengths
@@ -75,23 +72,26 @@ class MatlabConnectome:
 
   '''
   Returns max node value for a layer
-  ***Initializing offset is in here too***
   '''
   def max_child_node(self):
     for i in self._nodes:
       if i._layer in self._layer_max_child:
         continue
       self._layer_max_child[i._layer] = max(i._input_nodes)
+    self.create_offset_hash()
     return self._layer_max_child
 
-    '''
+  '''
+  Creates offset hash for referencing nodes in matrix
+  '''
+  def create_offset_hash(self):
     offset = 0
-    self.layer_offset[1] = offset
-    for i in range(2, len(layer_max) + 1):
-      if i == 2 : offset += layer_max[1]
-      offset += layer_max[i]
-      self.layer_offset[i] = offset
-    '''
+    self._layer_offset[1] = offset
+    for i in range(2, len(self._layer_max_child) + 1):
+      if i == 2 : offset += self._layer_max_child[1]
+      offset += self._layer_max_child[i]
+      self._layer_offset[i] = offset
+    return
 
   '''
   Prints all_paths to console
@@ -110,5 +110,9 @@ connect = MatlabConnectome(parsed_data._node_container)
 print(connect.layer_lens())
 print(connect.max_child_node())
 connect.construct_empty_matrix()
-print(len(connect._matrix))
-print(len(connect._matrix[0]))
+matrix = connect.fill_matrix();
+for i in matrix:
+  print i
+#print(connect._matrix)
+for i in range(1, len(connect._nodes)+1):
+  print(connect._hash_lookup[i])
