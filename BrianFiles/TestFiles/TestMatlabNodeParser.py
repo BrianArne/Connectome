175000da4e93e879c663afd8b8fdf91770d65449
Brian Arne <baarne@g.cofc.edu>
2019-01-18 13:14:49 -0500

    Implemeted a few MatlabParser test

--- BrianFiles/TestFiles/TestMatlabNodeParser.py
+++ BrianFiles/TestFiles/TestMatlabNodeParser.py
@@ -5,18 +5,57 @@ import Globals
 class TestMatlabNodeParser(unittest.TestCase):
 
   def setUp(self):
-    set_one_file_name = "SetOne.mat"
-    set_one_var_name = "SetOne"
-    set_one_data = MatlabNodeParser(Globals.TESTING_DIR, set_one_var_name)
+      set_one_file_name = "SetOne.mat"
+      set_one_var_name = "SetOne"
+      self.set_one_data = MatlabNodeParser(Globals.TESTING_DIR + set_one_file_name, set_one_var_name)
+
+      set_two_file_name = "SetTwo.mat"
+      set_two_var_name = "SetTwo"
+      self.set_two_data = MatlabNodeParser(Globals.TESTING_DIR+ set_two_file_name, set_two_var_name)
+
+      set_three_file_name = "SetThree.mat"
+      set_three_var_name = "SetThree"
+      self.set_three_data = MatlabNodeParser(Globals.TESTING_DIR + set_three_file_name, set_three_var_name)
+
+      set_test_file_name = "Test.mat"
+      set_test_var_name = "Test"
+      self.set_test_data = MatlabNodeParser(Globals.TESTING_DIR + set_test_file_name, set_test_var_name)
+
+      set_t_file_name = "T.mat"
+      set_t_var_name = "T"
+      self.set_t_data = MatlabNodeParser(Globals.TESTING_DIR + set_t_file_name, set_t_var_name)
+
+      self._containers = []
+      self._containers.append(self.set_one_data)
+      self._containers.append(self.set_two_data)
+      self._containers.append(self.set_three_data)
+      self._containers.append(self.set_test_data)
+      self._containers.append(self.set_t_data)
+
+      for c in self._containers:
+          c.load_data()
+          c.construct_node_container()
 
   def test_init(self):
-    pass
+
+      self.assertEqual(self.set_one_data._var_name,"SetOne")
+      self.assertEqual(self.set_two_data._var_name, "SetTwo")
+      self.assertEqual(self.set_three_data._var_name, "SetThree")
+      self.assertEqual(self.set_test_data._var_name, "Test")
+      self.assertEqual(self.set_t_data._var_name, "T")
+
+      self.assertEqual(self.set_one_data._file_name, Globals.TESTING_DIR + "SetOne.mat")
+      self.assertEqual(self.set_two_data._file_name, Globals.TESTING_DIR + "SetTwo.mat")
+      self.assertEqual(self.set_three_data._file_name, Globals.TESTING_DIR + "SetThree.mat")
+      self.assertEqual(self.set_test_data._file_name, Globals.TESTING_DIR + "Test.mat")
+      self.assertEqual(self.set_t_data._file_name, Globals.TESTING_DIR + "T.mat")
 
   # End test_init();
 
   def test_construct_node_container(self):
-    pass
-
+      for c in self._containers:
+          self.assertIsNotNone(c._node_container)
+          self.assertGreater(len(c._node_container), 0)
   # End construct_node_container();
 
   def test_get_data_layer(self):
